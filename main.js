717807d3ca811d478819565d567d7d55ab1b30b4
Demansell <demansell2016@gmail.com>
2023-01-31 21:34:49 -0600

    need to continue step 1

--- main.js
+++ main.js
@@ -4,39 +4,65 @@ const wizards = [
     name: 'Ron',
     house: 'Gryffindor',
     explelled: false,
+  }, 
+  {
+    id: 2,
+    name: 'Ron',
+    house: 'Gryffindor',
+    explelled: false,
   },
-]
+  {
+    id: 3,
+    name: 'Harry',
+    house: 'Gryffindor',
+    explelled: false,
+  },
+  {
+    id: 4,
+    name: 'Lockhart',
+    house: 'Ravenclaw',
+    explelled: false,
+  },
+  {
+    id: 5,
+    name: 'Draco',
+    house: 'Slytherin',
+    explelled: false,
+  },
+  {
+    id: 6,
+    name: 'Friar',
+    house: 'Hufflepuff',
+    explelled: false,
+  },
+  {
+    id: 7,
+    name: 'Todd',
+    house: 'Ravenclaw',
+    explelled: false,
+  },
+  {
+    id: 8,
+    name: 'Jason',
+    house: 'Hufflepuff',
+    explelled: false,
+  },
+];
+
 
 // Renders to Dom
-const render = (divId, htmlString) => {
-  const render = document.querySelector(divId);
-  render.innerHTML = htmlString;
-}
-
-// Sorting Student
-const sorting = (e) => {
-  event.preventDefault();
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const renderToDom = (divId, htmlToRender) => {
+const renderDom = (divId, htmlToRender) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = htmlToRender;
 };
+
+
+ 
+
+
+
+
+
+// add a student form need to pull html card
+// need to add new student card 
+//once i render to dom opening form need to make a event listern to make form show up 
