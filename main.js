e043b23a466ec95809bf21266ba6161d8f7e9320
Demansell <demansell2016@gmail.com>
2023-02-02 21:57:05 -0600

    added cards to dom need to work on query selector

--- main.js
+++ main.js
@@ -1,68 +1,66 @@
 const wizards = [
   {
     id: 1,
-    name: 'Ron',
-    house: 'Gryffindor',
-    explelled: false,
-  }, 
+    name: "Ron",
+    house: "Gryffindor",
+    expelled: false,
+  },
   {
     id: 2,
-    name: 'Ron',
-    house: 'Gryffindor',
-    explelled: false,
+    name: "Draco",
+    house: "Slytherin",
+    expelled: false,
   },
   {
     id: 3,
-    name: 'Harry',
-    house: 'Gryffindor',
-    explelled: false,
+    name: "Friar",
+    house: "Hufflepuff",
+    expelled: false,
   },
   {
-    id: 4,
-    name: 'Lockhart',
-    house: 'Ravenclaw',
-    explelled: false,
+    id: 4, 
+    name: "Lockhart",
+    house: "Ravenclaw",
+    expelled: false,
   },
-  {
-    id: 5,
-    name: 'Draco',
-    house: 'Slytherin',
-    explelled: false,
-  },
-  {
-    id: 6,
-    name: 'Friar',
-    house: 'Hufflepuff',
-    explelled: false,
-  },
-  {
-    id: 7,
-    name: 'Todd',
-    house: 'Ravenclaw',
-    explelled: false,
-  },
-  {
-    id: 8,
-    name: 'Jason',
-    house: 'Hufflepuff',
-    explelled: false,
-  },
+
 ];
 
+ // render to dom function
 
-// Renders to Dom
-const renderDom = (divId, htmlToRender) => {
+ const renderToDom = (divId, htmlToRender) => {
   const selectedDiv = document.querySelector(divId);
-  selectedDiv.innerHTML = htmlToRender;
+  selectedDiv.innerHTML = htmlToRender
 };
 
+let domString = "";
+  const cardsOnDom = (array) => {
+    let domString = "";
+    for (const wiz of array) {
 
- 
+      domString += `<div class="card" style="width: 18rem;">
+      <img src=${wiz.imageUrl} class="card-img-top" alt=${wiz.name}>
+      <div class="card-body">
+      <h5 class="card-title">${wiz.name}</h5>
+        <p class="card-text">${wiz.house}</p>
+        <button class="btn btn-danger" id="delete--${wiz.id}">Delete</button>
+      </div>   
+      </div>`;
+};
+
+renderToDom("#theWizards", domString);
+}
+
+cardsOnDom(wizards);
 
 
+const filter = (array, houses) => {
+  const wizardArray = [];
 
-
-
-// add a student form need to pull html card
-// need to add new student card 
-//once i render to dom opening form need to make a event listern to make form show up 
+  for (const wiz of array) {
+    if (wiz.house === houses) {
+      wizardArray.push(wiz)
+    }
+  }
+  return wizardArray;
+}
